9812bda2ee69f3ec06ba86011ffdcd1f06240b62
a-lysenko <aleksey.lysenko@gmail.com>
2020-04-17 16:04:33 +0300

    - add home route - move init page to home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent implements OnInit {
+  title = 'animal-management-fe';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
