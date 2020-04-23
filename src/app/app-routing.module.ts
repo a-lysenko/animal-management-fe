c43362985f062012e515f9ffd9ffaef763c2fc5f
a-lysenko <aleksey.lysenko@gmail.com>
2020-04-23 22:57:36 +0300

    - remove unneeded home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/home'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
+    redirectTo: '/animal'
   },
   {
     path: 'animal',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AnimalModule } from './animal/animal.module';
 import { CoreModule } from './_core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,8 +18,7 @@ import {OwnerEffects} from './_core/owner/owner.effects';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
